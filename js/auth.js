0aa7307a8bc0f5fa62106391c5e0d707986fe38a
nadafhanif8-svg <nadafhanif8@gmail.com>
2026-02-01 16:03:37 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,51 +1,23 @@
-// js/auth.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { 
-  getAuth, 
-  createUserWithEmailAndPassword, 
-  signInWithEmailAndPassword 
+import { auth } from "./firebase.js";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// 🔥 Firebase config (apna paste karo)
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "auragen.firebaseapp.com",
-  projectId: "auragen",
-  storageBucket: "auragen.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
-};
-
-// Init
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-// 🔐 SIGNUP
 window.signup = function () {
   const email = document.getElementById("signup-email").value;
   const password = document.getElementById("signup-password").value;
 
   createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      alert("Signup successful 🎉");
-      console.log(userCredential.user);
-    })
-    .catch((error) => {
-      alert(error.message);
-    });
+    .then(() => alert("Signup successful 🚀"))
+    .catch(err => alert(err.message));
 };
 
-// 🔐 LOGIN
 window.login = function () {
   const email = document.getElementById("login-email").value;
   const password = document.getElementById("login-password").value;
 
   signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      alert("Login successful 🚀");
-      console.log(userCredential.user);
-    })
-    .catch((error) => {
-      alert(error.message);
-    });
+    .then(() => alert("Login successful 🔥"))
+    .catch(err => alert(err.message));
 };
