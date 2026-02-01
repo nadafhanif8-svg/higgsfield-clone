244cd4f9f9f5448aa059d7a8ac2243ab5423bf84
nadafhanif8-svg <nadafhanif8@gmail.com>
2026-02-01 19:06:30 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -4,20 +4,20 @@ import {
   signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-window.signup = () => {
+window.signup = function () {
   const email = document.getElementById("signup-email").value;
   const password = document.getElementById("signup-password").value;
 
   createUserWithEmailAndPassword(auth, email, password)
-    .then(() => alert("Signup successful 🚀"))
+    .then(() => alert("Signup successful 🔥"))
     .catch(err => alert(err.message));
 };
 
-window.login = () => {
+window.login = function () {
   const email = document.getElementById("login-email").value;
   const password = document.getElementById("login-password").value;
 
   signInWithEmailAndPassword(auth, email, password)
-    .then(() => alert("Login successful 🔥"))
+    .then(() => alert("Login successful ✅"))
     .catch(err => alert(err.message));
 };
