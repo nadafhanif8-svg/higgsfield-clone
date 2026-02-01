5122545c0f97dbfb465523a9c1e26f6912cda567
nadafhanif8-svg <nadafhanif8@gmail.com>
2026-02-01 19:43:06 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { initializeApp, getApps, getApp } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+import { getAuth } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "XXXX",
-  authDomain: "auragen.firebaseapp.com",
-  projectId: "auragen",
-  appId: "XXXX"
+  apiKey: "AIzaSyD9Z0wtLs0GrrGOYayWXrOHZjvG6yumLBc",
+  authDomain: "auragen-2b036.firebaseapp.com",
+  projectId: "auragen-2b036",
+  storageBucket: "auragen-2b036.appspot.com",
+  messagingSenderId: "1034283501538",
+  appId: "1:1034283501538:web:4840b9458d45f522f7dbbb"
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 export const auth = getAuth(app);
